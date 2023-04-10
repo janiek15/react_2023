97ba2f89301e5a76553d7ea0a7f8b4847db2561f
janiek15 <namchin9@gamil.com>
2023-04-10 21:40:52 +0900

    버튼 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-
+import React, {useState} from "react";
 
 function App() {
+
+  const onBtnIncreaseClicked = () => {setNum(num + 1 );};
+
+  const onBtnDecreaseClicked = () => {setNum(num - 1 );};
+
+  const onBtnIncreaseTenClicked = () => {setNum(num + 10 );};
+
+  const onBtnDecreaseTenClicked = () => {setNum(num - 10 );};
+
+  const [num, setNum] = useState(0);
+
   return (
-    <div>안녕</div>
+    <>
+      NUM : {num}, <button onClick={onBtnIncreaseClicked}> +1 증가 </button>
+      <button onClick={onBtnDecreaseClicked}> -1 감소 </button>
+      <button onClick={onBtnIncreaseTenClicked}> +10 증가 </button>
+      <button onClick={onBtnDecreaseTenClicked}> -10 감소 </button>
+    </>
   );
+
 }
+
+
 
 export default App;
